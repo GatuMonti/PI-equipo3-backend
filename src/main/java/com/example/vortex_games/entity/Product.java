e249b5578db5b0f9c0bd34d9c55bf6a37f470575
Eduardo Jesus Monti <EduardoMonti1990@gmail.com>
2024-02-20 00:18:24 -0300

    configuracion del cors y adaptacion de la entidad producto al front

--- src/main/java/com/example/vortex_games/entity/Product.java
+++ src/main/java/com/example/vortex_games/entity/Product.java
@@ -29,11 +29,22 @@ public class Product {
 
     @NonNull
     @Column(nullable = false)
-    private String category;
+    private String description;
 
     @NonNull
     @Column(nullable = false)
-    private String description;
+    private double price;
+
+    @NonNull
+    @Column(nullable = false)
+    private String type;
+
+    @NonNull
+    @Column(nullable = false)
+    private String console;
+
+    @NonNull
+    private String category;
 
     @NonNull
     @Column(nullable = false)
@@ -49,6 +60,5 @@ public class Product {
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Image> images=new HashSet<>();
-
 
 }
