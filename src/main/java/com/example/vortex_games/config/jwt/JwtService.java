d4c2de9968093c5c13d0cc9d6e741d9d4626328f
Guillermo Abriola <guabriola@gmail.com>
2024-02-29 21:56:27 -0300

    Queda funcionando la devolucion del token con datos de usuario -> id usuario nombre y apellido

--- src/main/java/com/example/vortex_games/config/jwt/JwtService.java
+++ src/main/java/com/example/vortex_games/config/jwt/JwtService.java
@@ -1,16 +1,16 @@
 package com.example.vortex_games.config.jwt;
 
 
+import com.example.vortex_games.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,22 +21,26 @@ public class JwtService {
 
     private static final String SECRET_KEY="586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
 
-    public String getToken(UserDetails user) {
+    public String getToken(User user) {
         return getToken(new HashMap<>(), user);
     }
 
-    private String getToken(Map<String,Object> extraClaims, UserDetails user) {
+    private String getToken(Map<String,Object> extraClaims, User user) {
         return Jwts
                 .builder()
-                .setClaims(extraClaims)
-                .setSubject(user.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
-                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .claims(extraClaims)
+                .claim("userId", user.getId())
+                .claim("usuario", user.getUsername())
+                .claim("nombre", user.getNombre())
+                .claim("apellido", user.getApellido())
+                //Acá se saca el username---> .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                //Con esta linea le doy una expiración de un día---> .expiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey())
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[] keyBytes=Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
@@ -53,11 +57,11 @@ public class JwtService {
     private Claims getAllClaims(String token)
     {
         return Jwts
-                .parserBuilder()
-                .setSigningKey(getKey())
+                .parser()
+                .verifyWith(getKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public <T> T getClaim(String token, Function<Claims,T> claimsResolver)
