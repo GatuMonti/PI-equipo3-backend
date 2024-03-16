45c864fd744c6970a3fe5c9b570d7f3d4ef48f52
EmmanuelCastrillon <castrillon.rivas@gmail.com>
2024-03-15 23:57:39 -0500

    cambios

--- src/main/java/com/example/vortex_games/service/UserService.java
+++ src/main/java/com/example/vortex_games/service/UserService.java
@@ -1,10 +1,14 @@
 package com.example.vortex_games.service;
 
+import com.example.vortex_games.entity.Product;
 import com.example.vortex_games.entity.User;
+import com.example.vortex_games.repository.ProductRepository;
 import com.example.vortex_games.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,6 +17,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     //Methods Manual
 
     public Optional<User> buscarUsuarioPorName(String name){
@@ -20,4 +27,6 @@ public class UserService {
     }
 
     public Optional<User> buscarPorId(Integer id){ return userRepository.findById(id);}
+
+
 }
